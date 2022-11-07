e41c289921a1725b0c128d986ee0cfdb4a4b3396
Farid Hidayat <e.faridhidayat@gmail.com>
2022-11-07 20:41:19 +0700

    feat: routes home

---
+++ lib/app/extra/navigator/routes.dart
@@ -0,0 +1,13 @@
+import 'package:startapp/app/screens/home/home.dart';
+
+List routes = [
+  {'path': '/', 'screen': () => const HomeScreen()},
+  // {'path': '/splash', 'screen': () => SplashApp()},
+  // // ROUTES: Tasks
+  // {'path': '/login', 'screen': () => LoginScreen()},
+
+  // {'path': '/quran', 'screen': () => QuranHomeScreen()},
+  // {'path': '/quran/reader', 'screen': () => QuranReaderScreen()},
+
+  // {'path': '*', 'screen': () => NotfoundScreen()}
+];
